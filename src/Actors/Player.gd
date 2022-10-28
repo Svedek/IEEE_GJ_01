50aa6dd281144ca32ddf2600e432c7e77c841a56
naroon10 <naroon10@gmail.com>
2022-10-28 01:35:05 -0500

    Get rid of prints I guess

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -8,7 +8,6 @@ export var angular_drag:float = 0.1
 export var turn_speed:float = 1
 export var move_speed:float = 10
 
-
 var _velocity:Vector2 = Vector2.ZERO
 var _angular_velocity:float
 var _move_force:float
@@ -20,30 +19,15 @@ var _bullet_parent:Node
 
 
 func _ready():
-	#_bullet_parent = Node2D.new()
 	_bullet_parent = get_node("BulletParent")
-	print(_bullet_parent)
-	#get_tree().get_current_scene().add_child(_bullet_parent)
-	#print(get_tree())
-	#print(get_tree().get_root())
-	#print(get_tree().get_root().get_child(0))
-	#print(get_tree().get_current_scene())
-	
-	print(_bullet_parent.get_parent())
-	# Make bullet parent to be populated
-	pass
 
 func _process(delta):
 	handle_input()
 	handle_movement()
 
-	
-	
-	
 func _input(event):
 	if event.is_action_pressed("shoot"):
 		shoot()
-
 
 
 
@@ -57,7 +41,6 @@ func handle_input():
 func handle_movement():
 	_angular_velocity *= angular_drag
 	_velocity *= drag
-	
 	
 	_angular_velocity += _turn_force
 	var move_vector = Vector2(_move_force*cos(rotation), _move_force*sin(rotation))
