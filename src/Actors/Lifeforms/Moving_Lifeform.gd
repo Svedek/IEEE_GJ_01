e9678389c00381130fd23a696854ee1e24d7c1c4
naroon10 <naroon10@gmail.com>
2022-11-19 18:41:34 -0600

    implemented Moving_Lifeform
    moved player movement to Moving_Lifeform

--- src/Actors/Lifeforms/Moving_Lifeform.gd
+++ src/Actors/Lifeforms/Moving_Lifeform.gd
@@ -1,10 +1,10 @@
 extends "res://src/Actors/Lifeforms/Lifeform.gd"
 
 
-export var _drag:float = 0.1
-export var _angular_drag:float = 0.1
-export var _turn_speed:float = 1
-export var _move_speed:float = 10
+export(float, 0.0, 1.0) var _drag:float = 0.995 # 1.0 drag maintains full velocity *= drag
+export(float, 0.0, 1.0)  var _angular_drag:float = 0.975
+export var _turn_speed:float = 0.002
+export var _move_speed:float = 6
 
 onready var _kb = $"."
 
@@ -14,13 +14,14 @@ var _move_force:float
 var _turn_force:float
 
 
-func handle_input():
-	var move = Input.get_action_strength("move_forward")
-	var turn_dir = Input.get_axis("turn_left","turn_right")
-	
+func _physics_process(delta):
+	handle_movement() # possibly make use delta
+
+func set_move_intent(move:float, turn_dir:float):
 	_move_force = move * _move_speed
 	_turn_force = turn_dir * _turn_speed
 	
+# Assumes _move_force and _turn_force have already been set
 func handle_movement():
 	_angular_velocity *= _angular_drag
 	_velocity *= _drag
--- src/Actors/Lifeforms/Player/Player.gd
+++ src/Actors/Lifeforms/Player/Player.gd
@@ -1,18 +1,7 @@
-extends "res://src/Actors/Lifeforms/Lifeform.gd"
+extends "res://src/Actors/Lifeforms/Moving_Lifeform.gd"
 
 export var _player_bullet:PackedScene
 
-export var _drag:float = 0.995
-export var _angular_drag:float = 0.975
-export var _turn_speed:float = 0.002
-export var _move_speed:float = 6
-
-var _velocity:Vector2 = Vector2.ZERO
-var _angular_velocity:float
-var _move_force:float
-var _turn_force:float
-
-onready var _kb:KinematicBody2D = $"."
 onready var _firepoint:Node2D = $"Firepoint"
 var _bullet_parent:Node
 
@@ -23,8 +12,8 @@ func _ready():
 func _process(delta):
 	handle_input()
 
-func _physics_process(delta):
-	handle_movement() # TODO MAKE USE DELTA
+#func _physics_process(delta):
+#	handle_movement() # TODO MAKE USE DELTA
 
 func _input(event):
 	if event.is_action_pressed("shoot"):
@@ -36,18 +25,8 @@ func handle_input():
 	var move = Input.get_action_strength("move_forward")
 	var turn_dir = Input.get_axis("turn_left","turn_right")
 	
-	_move_force = move * _move_speed
-	_turn_force = turn_dir * _turn_speed
+	set_move_intent(move,turn_dir)
 	
-func handle_movement():
-	_angular_velocity *= _angular_drag
-	_velocity *= _drag
-	
-	_angular_velocity += _turn_force
-	var move_vector = Vector2(_move_force*cos(rotation), _move_force*sin(rotation))
-	
-	rotate(_angular_velocity)
-	_velocity = _kb.move_and_slide(_velocity+move_vector)
 	
 func shoot():
 	var bullet = _player_bullet.instance()
