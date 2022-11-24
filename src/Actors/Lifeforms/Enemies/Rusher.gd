8e43b8f23adf7596fd0927f27307e38aeb3f6f0f
Svedek <naroon10@gmail.com>
2022-11-23 23:41:59 -0600

    Rusher AI works!

--- src/Actors/Lifeforms/Enemies/Rusher.gd
+++ src/Actors/Lifeforms/Enemies/Rusher.gd
@@ -10,11 +10,24 @@ func _process(delta):
 	target_player()
 
 func target_player():
+	# Get radians to turn to be pointing at the player (-2PI <= turn_dir <= 2PI)
 	var to_player:Vector2 = PlayerInfo.get_player_position() - _kb.global_position
+	#	print("to_player: " + str(to_player))
 	var turn_dir:float = to_player.angle() - _kb.global_rotation
+	#	print("to_player.angle(): " + str(to_player.angle()))
+	#	print("_kb.global_rotation: " + str(_kb.global_rotation))
+	#	print("first turn_dir: " + str(turn_dir))
 	
+	# Ensure turn_dir is between -PI and PI
+	var loop:int = turn_dir / PI
+	turn_dir -= loop * (2*PI)
+	#	print("normalized turn_dir: " + str(turn_dir))
 	
-	#if i == 0:
-		
+	# Find turn intensity
+	var turn_intent:float = turn_dir / PI
+	var move_intent:float = (1 - abs(turn_intent))
+	move_intent *= move_intent
+	#	print("turn_intent: " + str(turn_intent))
+	#	print("move_intent: " + str(move_intent))
 	
-	#set_move_intent(move, turn_dir)
+	set_move_intent(move_intent, turn_intent)
